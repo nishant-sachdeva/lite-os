a0d9362ac2dbcd71292cd0fbcbebb6d2168dfd2a
nishant-sachdeva <nishant.sachdeva@research.iiit.ac.in>
2023-07-04 14:40:35 +0530

    added integration testing, support for should_panic tests

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,11 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(lite_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-mod vga_buffer;
-mod serial;
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-
-    exit_qemu(QemuExitCode::Success);
-}
+use lite_os::println;
 
 #[cfg(not(test))]
 #[panic_handler]
@@ -29,11 +18,7 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-
-    loop {}
+    lite_os::test_panic_handler(info)
 }
 
 #[no_mangle]
@@ -44,39 +29,4 @@ pub extern "C" fn _start() -> ! {
     test_main();
 
     loop {} // we need to loop forever because the _start function cannot return
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4); // the port used to exit QEMU
-        port.write(exit_code as u32);
-    }
-}
-
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
-}
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where T: Fn()
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
 }
