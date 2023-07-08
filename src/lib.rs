a22df678d56af7434ccba5056817424539b93887
nishant-sachdeva <nishant.sachdeva@research.iiit.ac.in>
2023-07-08 13:29:40 +0530

    added skeleton for exceptions. added breakpoint exception

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,15 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+#![feature(abi_x86_interrupt)]
 
 pub mod serial;
 pub mod vga_buffer;
+pub mod interrupts;
 
+pub fn init() {
+    interrupts::init_idt();
+}
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u32)]
@@ -58,6 +63,7 @@ pub fn test_panic_handler(info: &core::panic::PanicInfo) -> ! {
 #[cfg(test)]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    init();
     test_main();
 
     loop {} // we need to loop forever because the _start function cannot return
--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,11 @@ fn panic(info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    lite_os::init();
+
+    // invoke a breakpoint exception
+    x86_64::instructions::interrupts::int3();
+
     #[cfg(test)]
     test_main();
 
